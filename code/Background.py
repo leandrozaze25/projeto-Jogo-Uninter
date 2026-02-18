c51d625132cd28751ea3776d6ac221e59aedd48a
leandrozaze <leandro_zaze@hotmail.com>
2026-02-18 13:10:01 -0300

    metade da aula

--- code/Background.py
+++ code/Background.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Entity1 import Entity1
+from code.Entity import Entity
 
 
-class Background(Entity1):
-    def __init__(self):
-        pass
+class Background(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
         pass
